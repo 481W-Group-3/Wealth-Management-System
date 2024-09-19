a1d4033d70b02d3cb93901d03069f1ff6f451a9d
Justin-And <jandro@emich.edu>
2024-09-19 13:31:41 -0400

    Fixed an issue with account-creation.jsx that prevented building

--- front-wealth-app/src/components/user-create/account-creation.jsx
+++ front-wealth-app/src/components/user-create/account-creation.jsx
@@ -2,92 +2,89 @@ import "./account-creation.css";
 import React, { useState } from 'react';
 
 const UserCreate = () => {
+
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [userName, setUserName] = useState('');
     const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
+    const [password, setPassword] = useState('');
     const [repeatPassword, setRepeatPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         //if (true == true) {
-            //TODO Check if Email is in use when Database is implemented
-            console.log('First Name:', firstName);
-            console.log('Last Name:', lastName);
-            console.log('User Name:', userName);
-            console.log('Email:', email);
-            console.log('Password:', password);
+        //TODO Check if Email is in use when Database is implemented
+        console.log('First Name:', firstName);
+        console.log('Last Name:', lastName);
+        console.log('User Name:', userName);
+        console.log('Email:', email);
+        console.log('Password:', password);
         //}
         //else {
-            //display Error Text somewhere.
+        //display Error Text somewhere.
         //}
     };
-
     return (
         <form onSubmit={handleSubmit}>
-            // First Name
+            {/* First Name*/}
             <div>
-            <label>First Name:</label>
+                <label>First Name:</label>
             </div>
             <input type="text">
-                
 
+                onChange={(e) => setFirstName(e.target.value)}
             </input>
 
-            // Last Name
+            {/* Last Name*/}
             <div>
                 <label>Last Name:</label>
             </div>
             <input type="text">
 
+                onChange={(e) => setLastName(e.target.value)}
             </input>
 
-            // User Name
+            {/* User Name*/}
             <div>
                 <label>Username:</label>
             </div>
             <input type="text">
 
+                onChange={(e) => setUserName(e.target.value)}
             </input>
 
-            // Email
+            {/*Email*/}
             <div>
                 <label>Email:</label>
             </div>
             <input type="text">
 
+                onChange={(e) => setEmail(e.target.value)}
             </input>
 
-            // Password
+            {/* Password*/}
             <div>
                 <label>Password:</label>
             </div>
             <input type="text">
 
+                onChange={(e) => setPassword(e.target.value)}
             </input>
 
-            // Repeat Password
+            {/* Repeat Password */}
             <div>
                 <label>Re-enter Password</label>
             </div>
             <input type="text">
 
+                onChange={(e) => setRepeatPassword(e.target.value)}
             </input>
-
+        </form>
+    );
 
 
 
 
 };
 
-
-
-/*
-First Name
-Last Name
-Email
-Password
-Phone Number
-*/
 export default UserCreate;

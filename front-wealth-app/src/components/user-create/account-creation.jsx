0ba0bb674a9e16d62aec1506b03825af37027cbd
Justin-And <jandro@emich.edu>
2024-10-03 13:23:58 -0400

    Added conditionals for form submission button in account-creation-page. Fixed issues with formatting on account-creation-page.

--- front-wealth-app/src/components/user-create/account-creation.jsx
+++ front-wealth-app/src/components/user-create/account-creation.jsx
@@ -8,6 +8,19 @@ const UserCreate = () => {
     const [email, setEmail] = useState('');
     const role = "USER"
     const [password, setPassword] = useState('');
+    const [repassword, setRePassword] = useState('');
+
+    /*
+    Disables button if all forms are not filled, if 
+    password does not match re-entered password.
+    */
+        function SubmitButton(){
+            if (username && email && password && repassword && password == repassword){
+              return <button type="submit" id="checkout-submit-button">Submit</button>
+            } else {
+              return <button type="submit" id="checkout-submit-button" disabled>Submit</button>
+            };
+          };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -31,9 +44,9 @@ const UserCreate = () => {
     return (
         <div className="page-container">
             <div className={"create-account-container"}>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} id="submitform" name="submitform">
                     <div className={"in-form-container"}>
-                        <h2>Create Account</h2>
+                        <h2 id="create-account-h2">Create Account</h2>
                         <div className={"form-component"}>
                             <label htmlFor="username">Username:</label>
                             <input
@@ -64,7 +77,16 @@ const UserCreate = () => {
                             </input>
                         </div>
 
-                        <button type="submit">Submit</button>
+                        <div className={"form-component"}>
+                            <label htmlFor="repassword">Re-enter Password:</label>
+                            <input
+                                type="password"
+                                name="repassword"
+                                value={repassword}
+                                onChange={(e) => setRePassword(e.target.value)}>
+                            </input>
+                        </div>
+                        <SubmitButton/>
                     </div>
                 </form>
             </div>

667fe0d8b8ceafb1fc7ab422215ecf1f6850ba95
Shaun Lee <shaunljw32@gmail.com>
2024-10-22 01:04:37 -0400

    Fixed account-creation form styling

--- front-wealth-app/src/components/user-create/account-creation.jsx
+++ front-wealth-app/src/components/user-create/account-creation.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { createAccount } from "../../services/authService";
-import FormCards from '../user/FormsForDashboard.jsx'; 
-import welcomeImage from '../user/images/gradient.jpg'; 
+import FormCards from '../user/FormsForDashboard.jsx';
+import welcomeImage from '../user/images/gradient.jpg';
 
 const UserCreate = () => {
     const [username, setUsername] = useState('');
@@ -13,24 +13,20 @@ const UserCreate = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         if (!username || !email || !password) {
             setErrorMessage("Please fill in all the information");
             return;
         }
-
         if (password !== repassword) {
             setErrorMessage("Passwords do not match");
             return;
         }
-
         try {
             await createAccount({
                 'username': username,
                 'password': password,
                 'email': email
             });
-
             window.location.href = '/dashboard';
         } catch (error) {
             setErrorMessage("Username or Email already exists");
@@ -70,16 +66,20 @@ const UserCreate = () => {
     ];
 
     return (
-        <FormCards
-            handleText="Sign up and be one click away from managing your wealth!"
-            welcomeImage={welcomeImage}
-            handleSubmit={handleSubmit}
-            formFields={formFields}
-            showPassword={showPassword}
-            handleShowPassword={() => setShowPassword(!showPassword)}
-            errorMessage={error}
-            submitButtonText={username && email && password && repassword && password === repassword ? "Let's go!" : "Missing Fields or Password Mismatch"}
-        />
+        <div className="min-h-screen w-full">
+            <div className="w-[450px] mx-auto pt-20">
+                <FormCards
+                    handleText="Sign up and be one click away from managing your wealth!"
+                    welcomeImage={welcomeImage}
+                    handleSubmit={handleSubmit}
+                    formFields={formFields}
+                    showPassword={showPassword}
+                    handleShowPassword={() => setShowPassword(!showPassword)}
+                    errorMessage={error}
+                    submitButtonText={username && email && password && repassword && password === repassword ? "Let's go!" : "Missing Fields or Password Mismatch"}
+                />
+            </div>
+        </div>
     );
 };
 

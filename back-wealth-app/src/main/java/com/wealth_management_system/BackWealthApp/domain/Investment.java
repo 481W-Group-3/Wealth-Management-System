867d2d3c032e0121e840df7b3fd655441b6b48d5
harleen-khattra <hkhattra@emich.edu>
2024-10-28 15:13:13 -0400

    backend methods implemented for calculating investments

--- back-wealth-app/src/main/java/com/wealth_management_system/BackWealthApp/domain/Investment.java
+++ back-wealth-app/src/main/java/com/wealth_management_system/BackWealthApp/domain/Investment.java
@@ -1,6 +1,8 @@
 package com.wealth_management_system.BackWealthApp.domain;
 import jakarta.persistence.*;
 import jdk.jfr.DataAmount;
+
+import java.time.LocalDate;
 import java.util.*;
 
 
@@ -19,6 +21,38 @@ public class Investment {
 	private double principalInitial;
 	private double currentValue;
 	private double returns;
+	// Additional common fields
+    private String description; // Description of the investment
+    private LocalDate investmentDate; // Date of investment
+    private String investmentName; // Name of the investment or asset
+    private double fees; // Management or transaction fees
+    
+    // Fields specific to Stock
+    private double purchasePrice; // Purchase price of stock
+    private double annualDividend; // Annual dividend received
+    private int shares; // Number of shares owned
+
+    // Fields specific to Bond
+    private double faceValue; // Nominal value of bond
+    private double couponRate; // Interest rate of bond
+    private double pricePaid; // Price paid for the bond
+    private int yearsToMaturity; // Years until bond matures
+
+    // Fields specific to Mutual Fund
+    private double startingNAV; // Starting Net Asset Value
+    private double endingNAV; // Ending Net Asset Value
+    private double dividends; // Dividends received
+
+    // Fields specific to Real Estate
+    private double initialInvestment; // Amount initially invested
+    private double netRentalIncome; // Annual rental income
+    private double newPropertyValue; // Current market value of the property
+    private double originalPurchasePrice; // Price at which property was purchased
+
+    // Fields specific to Cryptocurrency
+    private double currentPrice; // Current market price of the cryptocurrenc
+    
+    
 	
 
 	//private Date currentDate;
@@ -31,6 +65,158 @@ public class Investment {
 	@OneToMany(mappedBy = "investment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private Set<Asset> asset;
 	*/
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getInvestmentDate() {
+		return investmentDate;
+	}
+
+	public void setInvestmentDate(LocalDate investmentDate) {
+		this.investmentDate = investmentDate;
+	}
+
+	public String getInvestmentName() {
+		return investmentName;
+	}
+
+	public void setInvestmentName(String investmentName) {
+		this.investmentName = investmentName;
+	}
+
+	public double getFees() {
+		return fees;
+	}
+
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+
+	public double getPurchasePrice() {
+		return purchasePrice;
+	}
+
+	public void setPurchasePrice(double purchasePrice) {
+		this.purchasePrice = purchasePrice;
+	}
+
+	public double getAnnualDividend() {
+		return annualDividend;
+	}
+
+	public void setAnnualDividend(double annualDividend) {
+		this.annualDividend = annualDividend;
+	}
+
+	public int getShares() {
+		return shares;
+	}
+
+	public void setShares(int shares) {
+		this.shares = shares;
+	}
+
+	public double getFaceValue() {
+		return faceValue;
+	}
+
+	public void setFaceValue(double faceValue) {
+		this.faceValue = faceValue;
+	}
+
+	public double getCouponRate() {
+		return couponRate;
+	}
+
+	public void setCouponRate(double couponRate) {
+		this.couponRate = couponRate;
+	}
+
+	public double getPricePaid() {
+		return pricePaid;
+	}
+
+	public void setPricePaid(double pricePaid) {
+		this.pricePaid = pricePaid;
+	}
+
+	public int getYearsToMaturity() {
+		return yearsToMaturity;
+	}
+
+	public void setYearsToMaturity(int yearsToMaturity) {
+		this.yearsToMaturity = yearsToMaturity;
+	}
+
+	public double getStartingNAV() {
+		return startingNAV;
+	}
+
+	public void setStartingNAV(double startingNAV) {
+		this.startingNAV = startingNAV;
+	}
+
+	public double getEndingNAV() {
+		return endingNAV;
+	}
+
+	public void setEndingNAV(double endingNAV) {
+		this.endingNAV = endingNAV;
+	}
+
+	public double getDividends() {
+		return dividends;
+	}
+
+	public void setDividends(double dividends) {
+		this.dividends = dividends;
+	}
+
+	public double getInitialInvestment() {
+		return initialInvestment;
+	}
+
+	public void setInitialInvestment(double initialInvestment) {
+		this.initialInvestment = initialInvestment;
+	}
+
+	public double getNetRentalIncome() {
+		return netRentalIncome;
+	}
+
+	public void setNetRentalIncome(double netRentalIncome) {
+		this.netRentalIncome = netRentalIncome;
+	}
+
+	public double getNewPropertyValue() {
+		return newPropertyValue;
+	}
+
+	public void setNewPropertyValue(double newPropertyValue) {
+		this.newPropertyValue = newPropertyValue;
+	}
+
+	public double getOriginalPurchasePrice() {
+		return originalPurchasePrice;
+	}
+
+	public void setOriginalPurchasePrice(double originalPurchasePrice) {
+		this.originalPurchasePrice = originalPurchasePrice;
+	}
+
+	public double getCurrentPrice() {
+		return currentPrice;
+	}
+
+	public void setCurrentPrice(double currentPrice) {
+		this.currentPrice = currentPrice;
+	}
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
